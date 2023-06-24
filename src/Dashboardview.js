96e8cf73571b67564e6a5126d57684ed37260dda
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-06-24 14:33:49 +0900

    tweak

--- src/Dashboardview.js
+++ src/Dashboardview.js
@@ -30,7 +30,7 @@ const Dashboardview = ({ user_id, baseurl }) => {
 
                 {/* <!-- Page Heading --> */}
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                    <h1 className="h3 mb-0 text-gray-800">LABLINK</h1>
                 </div>
                 {/* <!-- Content Row --> */}
 
