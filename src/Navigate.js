71243457e29700448365556d03855fdaeda329e8
Furuya Tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-10-29 22:19:13 +0900

    tweak

--- src/Navigate.js
+++ src/Navigate.js
@@ -134,6 +134,28 @@ const Navigate = ({ setisHomeValues, showNavigateValues, setshowNavigateValues, 
                                     <i className="fas fa-search fa-sm"></i>
                                     <span>論文検索</span></a>
                             </li>
+                            {/* <!-- Divider --> */}
+                            <hr className="sidebar-divider" />
+
+                            {/* <!-- Heading --> */}
+                            <div className="sidebar-heading">
+                                Link List
+                            </div>
+                            <li className="nav-item">
+                                <a className="nav-link" href='https://www.notion.so/takemura-lab/Takemura-Lab-82850b93f7944170ad2f2dcd33c14831?pvs=4'>
+                                    <i className="fas fa-solid fa-book"></i>
+                                    <span>Notion</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href='http://192.168.109.252:5000/'>
+                                    <i className="fas fa-solid fa-share"></i>
+                                    <span>NAS</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href='http://131.206.57.161/'>
+                                    <i className="fas fa-brands fa-gitlab"></i>
+                                    <span>gitlab</span></a>
+                            </li>
                             <hr className="sidebar-divider" />
                             <div class="sidebar-heading">
                                 Account
@@ -225,6 +247,28 @@ const Navigate = ({ setisHomeValues, showNavigateValues, setshowNavigateValues, 
                                 <a className="nav-link" onClick={showsearchpaper}>
                                     <i className="fas fa-search fa-sm"></i>
                                     <span>論文検索</span></a>
+                            </li>
+                            {/* <!-- Divider --> */}
+                            <hr className="sidebar-divider" />
+
+                            {/* <!-- Heading --> */}
+                            <div className="sidebar-heading">
+                                Link List
+                            </div>
+                            <li className="nav-item">
+                                <a className="nav-link" href='https://www.notion.so/takemura-lab/Takemura-Lab-82850b93f7944170ad2f2dcd33c14831?pvs=4'>
+                                    <i className="fas fa-solid fa-book"></i>
+                                    <span>Notion</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href='http://192.168.109.252:5000/'>
+                                    <i className="fas fa-solid fa-share"></i>
+                                    <span>NAS</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href='http://131.206.57.161/'>
+                                    <i className="fas fa-brands fa-gitlab"></i>
+                                    <span>gitlab</span></a>
                             </li>
                             <hr className="sidebar-divider" />
                             <div className="sidebar-heading">
