b168e3c4ad80fc7e1996ff718ca8907c9e2287f9
Furuya Tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-11-03 17:34:35 +0900

    tweak

--- src/Games/Tetris.js
+++ src/Games/Tetris.js
@@ -1,9 +1,7 @@
 import React from "react";
 import './Tetris.css';
 
-<script type="module">
-  import 'game-tetris/game-tetris.js';
-</script>
+const Tetris = require('react-tetris');
 
 const Tetriscontent = () => {
     return (
@@ -22,7 +20,46 @@ const Tetriscontent = () => {
                 </div>
             </div>
             {/* 以下のTetrisコンポーネントの記述 */}
-            <game-tetris></game-tetris>
+            <Tetris
+                keyboardControls={{
+                    down: 'MOVE_DOWN',
+                    left: 'MOVE_LEFT',
+                    right: 'MOVE_RIGHT',
+                    space: 'HARD_DROP',
+                    z: 'FLIP_COUNTERCLOCKWISE',
+                    x: 'FLIP_CLOCKWISE',
+                    up: 'FLIP_CLOCKWISE',
+                    p: 'TOGGLE_PAUSE',
+                    c: 'HOLD',
+                    shift: 'HOLD'
+                }}
+            >
+                {({
+                    HeldPiece,
+                    Gameboard,
+                    PieceQueue,
+                    points,
+                    linesCleared,
+                    state,
+                    controller
+                }) => (
+                    <div>
+                        <HeldPiece />
+                        <div>
+                            <p>Points: {points}</p>
+                            <p>Lines Cleared: {linesCleared}</p>
+                        </div>
+                        <Gameboard />
+                        <PieceQueue />
+                        {state === 'LOST' && (
+                            <div>
+                                <h2>Game Over</h2>
+                                <button onClick={controller.restart}>New game</button>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </Tetris>
         </div>
     );
 }
