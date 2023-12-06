674e3e70b9e5f406452d850905bb54bd0effcf3a
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-12-06 21:02:52 +0900

    tweak

--- src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
+++ src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
@@ -3,14 +3,19 @@ import React from "react";
 const Chatwithcodercontent = ({setmenu}) => {
     const chatbotlist = ["chatbot1", "chatbot2", "chatbot3", "chatbot4"]
 
+    const createchat = () => {
+       
+    }
+
     return (
         <div className="container my-3">
             <div className="row">
                 <h3 className="text-center mb-4">Chat With Coder</h3>
+                <button type="button" className="btn btn-primary text-white" onClick={createchat}>Create Chat</button>
                 {chatbotlist.map((chatbot, index) => (
                     <div key={index} className="col-6 mb-3">
                         <div className="p-3 border shadow rounded" style={{ backgroundColor: "#f5f5f5" }}>
-                            <p className="lead">{chatbot}</p>
+                            <p className="h2">{chatbot}</p>
                         </div>
                     </div>
                 ))}
--- src/Chatassistants/Chatwithdoc/Chatwithdoclist.js
+++ src/Chatassistants/Chatwithdoc/Chatwithdoclist.js
@@ -2,14 +2,18 @@ import React from "react";
 
 const Chatwithdoccontent = ({ setmenu }) => {
     const chatbotlist = ["chatbot1", "chatbot2", "chatbot3", "chatbot4"];
+    const createchat = () => {
+       
+    }
     return (
         <div className="container my-3">
             <div className="row">
                 <h3 className="text-center mb-4">Chat With Doc</h3>
+                <button type="button" className="btn btn-danger text-white" onClick={createchat}>Create Chat</button>
                 {chatbotlist.map((chatbot, index) => (
                     <div key={index} className="col-6 mb-3">
                         <div className="p-3 border shadow rounded" style={{ backgroundColor: "#f5f5f5" }}>
-                            <p className="lead">{chatbot}</p>
+                            <p className="h2">{chatbot}</p>
                         </div>
                     </div>
                 ))}
