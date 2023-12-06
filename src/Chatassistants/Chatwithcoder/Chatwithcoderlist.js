182a6467317557e1b74f552b526a3671752d8ecd
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-12-06 20:52:39 +0900

    tweak

--- src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
+++ src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
@@ -4,11 +4,12 @@ const Chatwithcodercontent = ({setmenu}) => {
     const chatbotlist = ["chatbot1", "chatbot2", "chatbot3", "chatbot4"]
 
     return (
-        <div className="container">
+        <div className="container my-3">
             <div className="row">
+                <h3 className="text-center mb-4">Chat With Doc</h3>
                 {chatbotlist.map((chatbot, index) => (
-                    <div key={index} className="col-6">
-                        <div className="p-3 border shadow" style={{ backgroundColor: "#f5f5f5" }}>
+                    <div key={index} className="col-6 mb-3">
+                        <div className="p-3 border shadow rounded" style={{ backgroundColor: "#f5f5f5" }}>
                             <p className="lead">{chatbot}</p>
                         </div>
                     </div>
