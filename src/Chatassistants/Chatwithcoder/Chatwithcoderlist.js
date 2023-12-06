9c40f94f0d5e8cd7d8f424f792a3038932be0443
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-12-06 20:56:19 +0900

    weak

--- src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
+++ src/Chatassistants/Chatwithcoder/Chatwithcoderlist.js
@@ -6,7 +6,7 @@ const Chatwithcodercontent = ({setmenu}) => {
     return (
         <div className="container my-3">
             <div className="row">
-                <h3 className="text-center mb-4">Chat With Doc</h3>
+                <h3 className="text-center mb-4">Chat With Coder</h3>
                 {chatbotlist.map((chatbot, index) => (
                     <div key={index} className="col-6 mb-3">
                         <div className="p-3 border shadow rounded" style={{ backgroundColor: "#f5f5f5" }}>
--- src/Chatassistants/Simplechat/Simplechatlist.js
+++ src/Chatassistants/Simplechat/Simplechatlist.js
@@ -5,7 +5,7 @@ const Simplechatcontent = ({ setmenu }) => {
     return (
         <div className="container my-3">
             <div className="row">
-                <h3 className="text-center mb-4">Chat With Doc</h3>
+                <h3 className="text-center mb-4">Simple Chat</h3>
                 {chatbotlist.map((chatbot, index) => (
                     <div key={index} className="col-6 mb-3">
                         <div className="p-3 border shadow rounded" style={{ backgroundColor: "#f5f5f5" }}>
